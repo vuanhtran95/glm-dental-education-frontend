00afd895097ddea0c9a0a01d48aaabd252fedb91
vu <vuanhtran95@gmail.com>
2025-01-29 10:32:52 +0000

    update

--- src/pages/help/desktop-help.tsx
+++ src/pages/help/desktop-help.tsx
@@ -9,16 +9,16 @@ import d8 from "../../assets/help-page/desktop/d8.png";
 
 const DesktopHelp = () => {
   return (
-    <div className="m-16">
+    <div className="m-auto w-[80vw]">
       <h6 className="text-xl">How to use the platform on desktop</h6>
-      <img className="m-16 border-2 border-indigo-900" src={d1}></img>
-      <img className="m-16 border-2 border-indigo-900" src={d2}></img>
-      <img className="m-16 border-2 border-indigo-900" src={d3}></img>
-      <img className="m-16 border-2 border-indigo-900" src={d4}></img>
-      <img className="m-16 border-2 border-indigo-900" src={d5}></img>
-      <img className="m-16 border-2 border-indigo-900" src={d6}></img>
-      <img className="m-16 border-2 border-indigo-900" src={d7}></img>
-      <img className="m-16 border-2 border-indigo-900" src={d8}></img>
+      <img className="my-16 border-2 border-indigo-900" src={d1}></img>
+      <img className="my-16 border-2 border-indigo-900" src={d2}></img>
+      <img className="my-16 border-2 border-indigo-900" src={d3}></img>
+      <img className="my-16 border-2 border-indigo-900" src={d4}></img>
+      <img className="my-16 border-2 border-indigo-900" src={d5}></img>
+      <img className="my-16 border-2 border-indigo-900" src={d6}></img>
+      <img className="my-16 border-2 border-indigo-900" src={d7}></img>
+      <img className="my-16 border-2 border-indigo-900" src={d8}></img>
     </div>
   );
 };
